2b849f17ed5c1de780cdcd8c9009f5cb0dc7d9fe
Anuradha-k-p <swapnalipawar587@gmail.com>
2023-08-25 21:25:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Display, { Buttontwo} from './Component/Display';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Display></Display>
+      <Buttontwo></Buttontwo>
+      <h1 className="fun"> Styling using Functional Component</h1>
+      {/* <header className="App-header"> */}
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p> */}
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+        {/* </p> */}
+        {/* <a */}
+{/* className="App-link" */}
+          {/* href="https://reactjs.org" */}
+          {/* target="_blank" */}
+          {/* rel="noopener noreferrer" */}
+        {/* > */}
+          {/* Learn React */}
+        {/* </a> */}
+      {/* </header> */}
     </div>
   );
 }
